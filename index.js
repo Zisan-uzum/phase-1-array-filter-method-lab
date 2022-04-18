fb26cd39f366eda6a5815f5eec4d3e683bfebbdc
zisan <uzummzisann@gmail.com>
2022-04-19 00:36:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+  );
+}
+function matchName(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
